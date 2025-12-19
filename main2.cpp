e839018e2a3718fb5fc102c8d4b5b47a4a26982c
680615521KhinChawSuLwin <khinchawsulwin161@gmail.com>
2025-12-19 16:16:40 +0700

    finishing part 2

--- main2.cpp
+++ main2.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
+#include<string>
 using namespace std; 
-int main(){
-    
+int main()
+{
+    string word;
+    cout<<"Enter a word: ";
+    getline(cin, word); //Read a line of text including spaces
+    bool palindrome=true;
+    int length=word.length();
+    for(int i=0; i<length/2; i++) //Check for palindrome
+    {
+        if(word[i]!=word[length-1-i]) //Compare characters
+        {
+            palindrome=false;
+            break;
+        }
+    }
+    if(palindrome)
+    {
+        cout<<"The word is a palindrome."<<endl;
+    }else
+    {
+        cout<<"The word is not a palindrome."<<endl;
+    }
     return 0;
 }
